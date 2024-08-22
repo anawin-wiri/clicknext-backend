7bc8de85679b76bd7f7735e1f30112ed3fcbc1b8
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2024-08-23 00:24:40 +0700

    authen

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  signIn(@Body() createUserDto: Record<string, any>) {
+    return this.authService.signIn(
+      createUserDto.username,
+      createUserDto.password,
+    );
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,33 @@
-import { Injectable, Dependencies } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-@Dependencies(UsersService)
 export class AuthService {
-  constructor(private usersService: UsersService) {} // Inject UsersService
-  // ฟังก์ชันอื่น ๆ
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService, // ตรวจสอบว่าคุณกำลัง inject JwtService อย่างถูกต้อง
+  ) {}
+
+  async signIn(
+    username: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOneByUsername(username);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const isMatch = password === user.userPassword;
+    if (!isMatch) {
+      throw new UnauthorizedException('Passwords do not match');
+    }
+    const payload = { sub: user };
+    return {
+      access_token: await this.jwtService.signAsync(payload), // ใช้งาน jwtService
+    };
+  }
 }
